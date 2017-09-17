10766520f3aca82f87742d96d283a9d46553a2de
David B. Templeton <dave@davetempleton.com>
2017-09-17 01:59:58 -0400

    Fixed file extension errors, error on output <1MiB

--- shrink-video.rb
+++ shrink-video.rb
@@ -4,6 +4,13 @@ require "fileutils"
 require "shellwords"
 require "yaml"
 
+# Generate file path without extension
+def no_ext(file)
+    name = File.basename(file, File.extname(file))
+    path = File.dirname(file)
+    File.join(path, name)
+end
+
 # Validate config and set up files
 config_path = File.join(File.dirname(File.expand_path(__FILE__)),"config.yml")
 abort "config.yml file does not exist in same folder as script" unless File.exist?(config_path)
@@ -13,6 +20,7 @@ abort "Directory trash_path does not exist" unless Dir.exist?(config['trash_path
 abort "dotfile_skip must begin with period" unless config['dotfile_skip'][0,1] == "."
 abort "All extensions must begin with period" unless config['extensions'].all?{ |ext| ext[0,1] == "." }
 FileUtils.touch(config['checked_path']) unless File.exist?(config['checked_path'])
+FileUtils.touch(config['error_path']) unless File.exist?(config['error_path'])
 
 # Do not run if already running or exited with uncaught exception
 abort "RUNNING file present" if File.exist?(config['running_path'])
@@ -32,10 +40,16 @@ Dir.glob("**/*") do |filename|
     next if File.exist?(File.join(File.expand_path("..", File.dirname(filename)),config['dotfile_skip']))
     # Skip if file doesn't have required extension
     next unless config['extensions'].include?(File.extname(filename).downcase)
-    # Skip if already in checked list
-    next if File.readlines(config['checked_path']).grep(filename).any?
     # Skip if created in delay_days
     next if Time.now - File.ctime(filename) < config['delay_days'].to_i * 86400
+    
+    # Skip if already in checked list
+    in_checked = false
+    filename_no_ext = no_ext(filename)
+    File.readlines(config['checked_path']).each do |line|
+        in_checked = true if filename_no_ext == no_ext(line)
+    end
+    next if in_checked
     
     # Add to checked list
     File.open(config['checked_path'],"a") { |f| f.puts(filename) }
@@ -76,10 +90,12 @@ Dir.glob("**/*") do |filename|
         namebase = File.basename(filename, extbase)
         pathbase = File.dirname(filename)
         
-        # Output file is stored here temporarily
+        # Create paths for output file temporary and final locations
         working_file_path = File.join(config['working_path'], namebase) + ".mkv"
+        filename_out = File.join(pathbase, namebase) + ".mkv"
         # Escape filename for security
         working_file_path_s = Shellwords.escape(working_file_path)
+        filename_out_s = Shellwords.escape(filename_out)
         
         # HandBrakeCLI command below. Two channel AAC audio only, x264 encoder, quality level 25, passthrough subtitles, MKV container.
         handbrake_cmd = "HandBrakeCLI -m -E ffaac -B 128 -6 stereo -X #{encode_width} --loose-crop -e x264 -q 25 --x264-preset medium -s 1,2,3,4,5 -f mkv -i #{filename_s} -o #{working_file_path_s}"
@@ -88,6 +104,15 @@ Dir.glob("**/*") do |filename|
         # Move on if HandBrakeCLI has non-zero exit code
         if `$?`.to_i != 0
             File.open(config['error_path'],"a") { |f| f.puts("Handbrake error: #{filename}") }
+            File.unlink(config['running_path'])
+            next
+        end
+        
+        # Move on if under 1MiB in size
+        output_size = File.size(working_file_path).to_f / 2**20
+        if output_size < 1
+            File.open(config['error_path'],"a") { |f| f.puts("Output under 1MiB, aborted: #{filename}") }
+            File.unlink(working_file_path)
             File.unlink(config['running_path'])
             next
         end
@@ -107,16 +132,16 @@ Dir.glob("**/*") do |filename|
             next
         else
             # Only move output file from temporary folder if input file was moved successfully
-            FileUtils.mv(working_file_path, filename)
+            FileUtils.mv(working_file_path, filename_out)
         end
         
         # Add file to transcoded list
         File.open(config['transcoded_path'],"a") { |f| f.puts(filename) }
         
         # Find output video stats
-        new_bitrate = `ffprobe -v error -select_streams v:0 -show_entries format=bit_rate -of default=noprint_wrappers=1:nokey=1 #{filename_s}`.chomp.to_i
-        new_width = `ffprobe -v error -select_streams v:0 -show_entries stream=width -of default=noprint_wrappers=1:nokey=1 #{filename_s}`.chomp.to_i
-        new_height = `ffprobe -v error -select_streams v:0 -show_entries stream=height -of default=noprint_wrappers=1:nokey=1 #{filename_s}`.chomp.to_i
+        new_bitrate = `ffprobe -v error -select_streams v:0 -show_entries format=bit_rate -of default=noprint_wrappers=1:nokey=1 #{filename_out_s}`.chomp.to_i
+        new_width = `ffprobe -v error -select_streams v:0 -show_entries stream=width -of default=noprint_wrappers=1:nokey=1 #{filename_out_s}`.chomp.to_i
+        new_height = `ffprobe -v error -select_streams v:0 -show_entries stream=height -of default=noprint_wrappers=1:nokey=1 #{filename_out_s}`.chomp.to_i
         
         # Log if output file is still above threshold, or if we managed to create a larger file
         if ( ( new_bitrate.to_f / ( new_width * new_height ) ) > adjusted_threshold ) || ( new_bitrate > bitrate )
