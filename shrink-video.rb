c69ad34d8f9fcb6c3b70dcfcb3fe8ac61f164187
David B. Templeton <dave@davetempleton.com>
2018-04-14 09:51:50 -0400

    More validation, fixed exit status code

--- shrink-video.rb
+++ shrink-video.rb
@@ -26,16 +26,18 @@ abort "Directory working_path does not exist" unless Dir.exist?(config['working_
 abort "Directory trash_path does not exist" unless Dir.exist?(config['trash_path'])
 abort "dotfile_skip must begin with period" unless config['dotfile_skip'][0,1] == "."
 abort "Thresholds must be numbers" unless ( config['threshold'].is_a? Numeric ) && ( config['threshold_sm'].is_a? Numeric )
-abort "max_width must be a number" unless config['max_width'].is_a? Numeric
+abort "max_width must be a integer" unless config['max_width'].is_a? Integer
+abort "max_width must be between 1000-10000, exclusive" unless ( config['max_width'].to_i > 1000 ) && ( config['max_width'].to_i < 10000 )
+abort "delay_days should be non-negative number" unless ( config['delay_days'].is_a? Numeric ) && ( config['delay_days'].to_f >= 0 )
 unless ( config['encode_quality'].is_a? Integer ) && ( config['encode_quality'] >= 18 ) && ( config['encode_quality'] <= 28 )
-    abort "encode_quality must be an integer between 18-28"
+    abort "encode_quality must be an integer between 18-28, inclusive"
 end
 abort ("Not a valid x264 encoder speed; must be one of:\n" + speeds.to_s) unless speeds.include? config['speed']
 unless config['extensions'].all?{ |ext| ext[0,1] == "." } && config['mandatory_encode'].all?{ |ext| ext[0,1] == "." }
     abort "All extensions must begin with period"
 end
-extensions.each { |ext| ext.downcase! }
-mandatory_encode.each { |ext| ext.downcase! }
+config['extensions'].each { |ext| ext.downcase! }
+config['mandatory_encode'].each { |ext| ext.downcase! }
 config['mandatory_encode'].each do |ext|
     abort "All extensions that must be mandatorily encoded must also be in the extensions array" unless config['extensions'].include? ext
 end
@@ -46,7 +48,7 @@ FileUtils.touch(config['error_path']) unless File.exist?(config['error_path'])
 abort "RUNNING file present" if File.exist?(config['running_path'])
 
 # Print start message, initialize counter
-puts "Starting " + Time.now.to_s
+puts "Starting #{Time.now}"
 counter = 0
 
 # Run from current directory if argument is not passed
@@ -68,7 +70,7 @@ Dir.glob("**/*") do |filename|
     # Skip if file doesn't have required extension
     next unless config['extensions'].include?(File.extname(filename).downcase)
     # Skip if created in delay_days
-    next if Time.now - File.ctime(filename) < config['delay_days'].to_i * 86400
+    next if Time.now - File.ctime(filename) < config['delay_days'].to_f * 86400
     
     # Skip if already in checked list
     in_checked = false
@@ -108,6 +110,7 @@ Dir.glob("**/*") do |filename|
         
         # Create RUNNING file
         File.open(config['running_path'],"w") { |f| f.puts(filename) }
+        puts "Transcoding: #{filename}"
         
         # Set output video width. Max video size is 720p
         width > config['max_width'].to_i ? encode_width = config['max_width'].to_i : encode_width = width
@@ -125,12 +128,12 @@ Dir.glob("**/*") do |filename|
         
         # HandBrakeCLI command below. Two channel AAC audio only, x264 encoder, passthrough subtitles, MKV container.
         handbrake_cmd = "HandBrakeCLI -m -E ffaac -B 128 -6 stereo -X #{encode_width} --loose-crop -e x264 -q #{config['encode_quality']} --x264-preset #{config['speed']} -s 1,2,3,4,5 -f mkv -i #{filename_s} -o #{working_file_path_s}"
-        puts "Transcoding: " + filename
         `#{handbrake_cmd} > /dev/null 2>&1`
         
-        # Move on if HandBrakeCLI has non-zero exit code, potentially abandoning output file
-        if `$?`.to_i != 0
+        # Move on if HandBrakeCLI has non-zero exit code, deleting any output file
+        unless $?.success?
             File.open(config['error_path'],"a") { |f| f.puts("Handbrake error: #{filename}") }
+            File.unlink(working_file_path) if File.exist?(working_file_path)
             File.unlink(config['running_path'])
             next
         end
@@ -208,5 +211,5 @@ Dir.glob("**/*") do |filename|
     
 end
 
-puts "Transcoded " + counter + " file(s)."
+puts "Transcoded #{counter} file(s)."
 puts "Exiting."
